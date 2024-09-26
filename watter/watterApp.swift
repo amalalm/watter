120f6d167b0e167672c7e924900a29056f395c30
amalalm <amalmoh.mtr@gmail.com>
2024-09-26 17:16:57 +0300

    Initial Commit

---
+++ watter/watterApp.swift
@@ -0,0 +1,17 @@
+//
+//  watterApp.swift
+//  watter
+//
+//  Created by Amal almutairi on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct watterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
